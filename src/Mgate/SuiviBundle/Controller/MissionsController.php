11fece05e37ae878c271b14eec9792a8fa2a81e3
stoakes <Stoakes@users.noreply.github.com>
2017-06-15 10:44:37 +0200

    Remove unused index page in MissionSController
    Ping #53

--- src/Mgate/SuiviBundle/Controller/MissionsController.php
+++ src/Mgate/SuiviBundle/Controller/MissionsController.php
@@ -25,19 +25,6 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class MissionsController extends Controller
 {
-    /**
-     * @Security("has_role('ROLE_SUIVEUR')")
-     */
-    public function indexAction($page)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entities = $em->getRepository('MgateSuiviBundle:Etude')->findAll();
-
-        return $this->render('MgateSuiviBundle:Etude:index.html.twig', array(
-            'etudes' => $entities,
-        ));
-    }
 
     /**
      * @Security("has_role('ROLE_SUIVEUR')")
