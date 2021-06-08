dfb409262f7669af71cebb6546fb25a5f713e756
rbonrepa <remi.bonrepaux09@gmail.com>
2021-06-08 16:24:31 +0200

    add tab "génération de document"

---
+++ src/Controller/GenerationDocController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GenerationDocController extends AbstractController
+{
+    /**
+     * @Route("/generation/doc", name="generation_doc")
+     */
+    public function index(): Response
+    {
+        return $this->render('generation_doc/index.html.twig', [
+            'controller_name' => 'GenerationDocController',
+        ]);
+    }
+}
