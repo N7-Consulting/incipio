f8aeb83560d7da7e6c18857dff6ce51c7d05c141
rbonrepa <remi.bonrepaux09@gmail.com>
2021-06-09 14:36:34 +0200

    Dans la page Archivage: lister les docs de la BDD

--- src/Controller/GestionDocuments/ArchivageDocController.php
+++ src/Controller/GestionDocuments/ArchivageDocController.php
@@ -5,6 +5,8 @@ namespace App\Controller\GestionDocuments;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Publish\Document;
+  
 
 class ArchivageDocController extends AbstractController
 {
@@ -12,9 +14,18 @@ class ArchivageDocController extends AbstractController
      * @Route("archivage/doc", name="archivage_doc")
      */
     public function index(): Response
-    {
+    {   $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository(Document::class)->findAll();
+
+        $totalSize = 0;
+        foreach ($entities as $entity) {
+            $totalSize += $entity->getSize();
+        }
         return $this->render('GestionDocuments/archivage_doc/index.html.twig', [
             'controller_name' => 'ArchivageDocController',
+            'docs' => $entities,
+            'totalSize' => $totalSize,
         ]);
     }
 }
