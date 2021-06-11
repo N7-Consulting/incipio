9491f4fe3e7fff9192d81ad70ae4ba682d206a78
rbonrepa <remi.bonrepaux09@gmail.com>
2021-06-11 16:05:32 +0200

    debug

--- src/Controller/StockDocuments/ChiffrageController.php
+++ src/Controller/StockDocuments/ChiffrageController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\GestionDocuments;
+namespace App\Controller\StockDocuments;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- src/Controller/StockDocuments/TresoController.php
+++ src/Controller/StockDocuments/TresoController.php
@@ -1,32 +1,32 @@
 <?php
 
-namespace App\Controller\GestionDocuments;
+namespace App\Controller\StockDocuments;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Treso\Facture; 
 use App\Entity\Publish\Document;
-use App\Entity\Project\Etude;
 
-class ActicoController extends AbstractController
+class TresoController extends AbstractController
 {
     const docEtude = ['CDC', 'PC', 'CE', 'CCA', 'BDC', 'RM', 'AVRM', 'AVCE', 'PVRI', 'PVRF', 'QS' ];
 
     /**
-     * @Route("actico", name="actico")
+     * @Route("treso", name="treso")
      */
     public function index(): Response
     {   
         $docEtude = self::docEtude;
         $em = $this->getDoctrine()->getManager();
+        $factures = $em->getRepository(Facture::class)->getFactures();
         $entities = $em->getRepository(Document::class)->findAll();
-        $etudes = $em->getRepository(Etude::class)->findAll();
 
-        return $this->render('StockDocuments/actico/index.html.twig', [
-            'controller_name' => 'ActicoController',
-            'docs' => $entities,
+        return $this->render('StockDocuments/treso/index.html.twig', [
+            'controller_name' => 'TresoController',
+            'factures' => $factures,
             'docEtude' => $docEtude,
-            'etudes' => $etudes,
+            'docs' => $entities,
         ]);
     }
 }
