ff21ab71eafc855b7fdbcd3565bf78ad13922fd0
rbonrepa <remi.bonrepaux09@gmail.com>
2021-06-12 03:04:43 +0200

    Architecture du nouvel onglet tableau des processus

---
+++ src/Controller/Stat/TabProcessController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Stat;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TabProcessController extends AbstractController
+{
+    /**
+     * @Route("/tab/process", name="tab_process")
+     */
+    public function index(): Response
+    {
+        return $this->render('tab_process/index.html.twig', [
+            'controller_name' => 'TabProcessController',
+        ]);
+    }
+}
