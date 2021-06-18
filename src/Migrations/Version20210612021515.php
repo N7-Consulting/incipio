61125b7516039813004bbf361f154321cbc1bcc6
Yann Lacroix <yann.lacroix.dev@gmail.com>
2021-06-18 17:17:55 +0200

    version conflits after merge

--- src/Migrations/Version20210612021515.php
+++ src/Migrations/Version20210612021515.php
@@ -10,38 +10,26 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:src/Migrations/Version20210612021515.php
 final class Version20210612021515 extends AbstractMigration
-=======
-final class Version20210613185604 extends AbstractMigration
->>>>>>> document:src/Migrations/Version20210613185604.php
 {
-    public function getDescription() : string
-    {
-        return '';
-    }
+        public function getDescription(): string
+        {
+                return '';
+        }
 
-    public function up(Schema $schema) : void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        public function up(Schema $schema): void
+        {
+                // this up() migration is auto-generated, please modify it to your needs
+                $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-<<<<<<< HEAD:src/Migrations/Version20210612021515.php
-        $this->addSql('CREATE TABLE Processus (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, pilote VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-=======
-        $this->addSql('ALTER TABLE Etude ADD cdc VARCHAR(255) DEFAULT NULL, ADD pc VARCHAR(255) DEFAULT NULL, ADD cca VARCHAR(255) DEFAULT NULL, ADD bdc VARCHAR(255) DEFAULT NULL, ADD qs VARCHAR(255) DEFAULT NULL');
->>>>>>> document:src/Migrations/Version20210613185604.php
-    }
+                $this->addSql('CREATE TABLE Processus (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, pilote VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        }
 
-    public function down(Schema $schema) : void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        public function down(Schema $schema): void
+        {
+                // this down() migration is auto-generated, please modify it to your needs
+                $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-<<<<<<< HEAD:src/Migrations/Version20210612021515.php
-        $this->addSql('DROP TABLE Processus');
-=======
-        $this->addSql('ALTER TABLE Etude DROP cdc, DROP pc, DROP cca, DROP bdc, DROP qs');
->>>>>>> document:src/Migrations/Version20210613185604.php
-    }
+                $this->addSql('DROP TABLE Processus');
+        }
 }
