8f91e862c9919338f5e38fe91b8b6c673d5f3c4f
Yann Lacroix <yann.lacroix.dev@gmail.com>
2021-07-02 17:09:17 +0200

    ajout nom to form

--- src/Form/Project/CcaType.php
+++ src/Form/Project/CcaType.php
@@ -8,6 +8,7 @@ use App\Form\Personne\ProspectType;
 use Genemu\Bundle\FormBundle\Form\JQuery\Type\Select2EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,11 +17,15 @@ class CcaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('nom', TextType::class, [
+                'label' => 'suivi.cca_form.nom_interne',
+                'translation_domain' => 'project',
+            ])
             ->add('knownProspect', CheckboxType::class, [
                 'label' => 'suivi.etude_form.client_bdd',
                 'translation_domain' => 'project',
                 'required' => false,
-                ])
+            ])
             ->add('prospect', Select2EntityType::class, [
                 'class' => Prospect::class,
                 'choice_label' => 'nom',
