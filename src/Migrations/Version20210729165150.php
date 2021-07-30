105021dc335ea2e8e04c12310a1336bc5fdf67a4
Yann Lacroix <yann.lacroix.dev@gmail.com>
2021-07-30 15:11:05 +0200

    Bdc integrated into Ce

---
+++ src/Migrations/Version20210729165150.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210729165150 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Ce ADD type SMALLINT NOT NULL');
+        // Update all Ce to get a type
+        $this->addSql("UPDATE Ce SET type=0 WHERE type=NULL");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Ce DROP type');
+    }
+}
