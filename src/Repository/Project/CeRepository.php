431a9e890220578cf8e7c29f428be072fc58e308
Yann Lacroix <yann.lacroix.dev@gmail.com>
2021-07-30 19:19:19 +0200

    hotfix/ too many parameters

--- src/Repository/Project/CeRepository.php
+++ src/Repository/Project/CeRepository.php
@@ -16,9 +16,9 @@ class CeRepository extends EntityRepository
     ->from(Ce::class, 'bdc')
     ->where('bdc IS NOT NULL')
     ->andWhere('bdc.type = :typeBdc')
-    ->setParameter('typeBdc', $typeBdc)
     ->join('bdc.etude', 'e')
-    ->where('e.cca = :id')
+    ->andWhere('e.cca = :id')
+    ->setParameter('typeBdc', $typeBdc)
     ->setParameter('id', $id)
     ->orderBy('bdc.dateSignature', 'DESC');
     $query = $qb->getQuery();
